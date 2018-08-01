5f0a1db50def99f7a18362462692178339c8ff74
Peter Schwarck <peterschwarck@gmail.com>
2018-08-01 16:13:16 +0000

    updated with readme

--- src/js/index.js
+++ src/js/index.js
@@ -9,43 +9,30 @@ import 'bootstrap';
 //include your own styles
 import '../style/index.scss';
 
+
+
 window.onload = function(){
-    console.log('Hello Rigo from the console!');
-    document.querySelector('.error').style.display = "none";
+    document.querySelector("#daButton").addEventListener("click", generateCard);
+    document.querySelector('#daButton').innerHTML = "<h1>" + 'Click Here' + '</h1>';
+     generateCard();
 };
 
-var randomSuit = Math.floor(Math.random() * 4);
-var randomNum = Math.floor(Math.random() * 13);
-
-
-var arrayNumbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-var arraySuit = ["https://www.wpclipart.com/signs_symbol/assorted/playing_card_icons/card_diamond.png", "https://www.homemade-gifts-made-easy.com/image-files/homemade-valentine-card-heart-template-800x524.gif", 
-"https://cdn3.iconfinder.com/data/icons/basic-mobile-part-2/512/clubs-256.png", "https://image.freepik.com/free-icon/spades-symbol_318-40683.jpg"
-];
-//var arrayColor = ['#FF0000', '#000000'];
-
-
-var images = document.getElementsByTagName("img");
-//console.log(images.length);
-for (let i=0; i<images.length; i++) {
-    images[i].src = arraySuit[randomSuit]; 
-
-    }
+function generateCard(){
+    var randomSuit = Math.floor(Math.random() * 4);
+    var randomNum = Math.floor(Math.random() * 13);
     
-if (randomSuit===2 || randomSuit ===3) {
-    console.log("black");
-    } else 
-    {
-    console.log("red");
+    
+    var arrayNumbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    var arraySuit = ["diamond", "heart", "club", "spade"];
+    
+    
+    var images = document.querySelector(".symbol");
+    
+    var h2s = document.querySelectorAll("h2");
+    for (let i=0; i<h2s.length; i++) {
+        h2s[i].innerHTML = arrayNumbers[randomNum];
     }
-
-var h2s = document.getElementsByTagName("h2");
-for (let i=0; i<h2s.length; i++) {
-    h2s[i].innerHTML = arrayNumbers[randomNum];
+    document.querySelector(".mainDiv").classList.add(arraySuit[randomSuit]);
+    return null;
 }
 
-//for (let x=0; x<100; x++) {
-    console.log(Math.floor(Math.random() * 13));
-//}
-//console.log(randomSuit);
-//console.log(randomNum);
